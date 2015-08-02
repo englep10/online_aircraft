54cd8b229f2f6e47e9408e96df119c7675912262
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-08-02 17:05:57 -0400

    refactored code and added messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
-  devise_for :installs
   get 'pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
